3dfef9cdf30d35ef2326cd6fd4bbc2db35722781
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-05-26 23:08:20 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ByronCalderon3A_Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Suma suma= new Suma();
+            suma.Calcular();
+        }
+    }
+}
